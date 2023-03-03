e77030d172e39b157edbda40c311aeaced1793f9
Matt Wilde <mwilde345@gmail.com>
2023-03-03 12:26:20 -1000

    comments. synchronous promise chain. sleep between requests. log statements

--- delete-friends.js
+++ delete-friends.js
@@ -1,7 +1,14 @@
 token = "YOUR_TOKEN_HERE";
 
+// how many milliseconds to wait between requests to avoid throttling.
+sleepTime = 500;
+
+sleep = async () => {
+  return new Promise((resolve) => setTimeout(resolve, sleepTime));
+};
+
 // Set with your own list
-const listOfFriendsToDelete = `
+listOfFriendsToDelete = `
 friend1,
 friend2,
 friend3
@@ -23,11 +30,13 @@ getGamerTagID = async (gamerTag) => {
   })
     .then((result) => {
       return result.json().then((data) => {
-        return data.PrimaryArea.Regions[0].Modules[0].DetailViewModel.Id;
+        const id = data.PrimaryArea.Regions[0].Modules[0].DetailViewModel.Id;
+        console.log("SUCCESS id for ", gamerTag, " is ", id);
+        return id;
       });
     })
     .catch((e) => {
-      console.log("failed to get gamertag ID", e);
+      console.error("FAIL to get gamertag ID for", gamerTag);
     });
 };
 
@@ -48,44 +57,45 @@ deleteSingleFriend = async (id) => {
     mode: "cors",
   })
     .then((result) => {
+      console.log("SUCCESS delete friend " + id);
       return result.ok;
     })
     .catch((e) => {
-      console.log("failed to delete friend" + id, e);
+      console.error("FAIL to delete friend " + id);
     });
 };
 
 // Get IDs for a comma separated list of gamertags
 getIDs = async (friendsToDelete) => {
-  const ids = await Promise.all(
-    friendsToDelete.map((tag) => {
-      return new Promise((res) => {
-        return res(getGamerTagID(tag));
-      });
-    })
-  ).then((result) => {
-    return result;
-  });
+  const ids = [];
+  for (let i = 0; i < friendsToDelete.length; i++) {
+    const tag = friendsToDelete[i];
+    let id = await getGamerTagID(tag);
+    ids.push(id);
+    console.log("...");
+    await sleep();
+  }
   return ids;
 };
 
 // Get IDs for gamertags and use them to step through each one and delete friends
 deleteFriends = async () => {
+  console.log("Starting deletion, please wait...");
   const deletingArray = listOfFriendsToDelete.replace("\n", "").split(",");
   const ids = await getIDs(deletingArray);
-  return await Promise.all(
-    ids.map((id) => {
-      return new Promise((res) => {
-        return res(deleteSingleFriend(id));
-      });
-    })
-  ).then((result) => {
-    const successfullyDeletedCount = result.filter((r) => !!r).length;
-    console.log(
-      `Done! Deleted ${successfullyDeletedCount} of ${deletingArray.length}`
-    );
-    return true;
-  });
+  console.log(
+    "Finished fetching IDs for all gamertags in deletion list. If some failed, delete those manually. Deleting the rest now. Please wait..."
+  );
+  let deletedCount = 0;
+  for (let i = 0; i < ids.length; i++) {
+    const id = ids[i];
+    const successfulDeletion = await deleteSingleFriend(id);
+    deletedCount += successfulDeletion ? 1 : 0;
+    console.log("...");
+    await sleep();
+  }
+  console.log(`Done! Deleted ${deletedCount} of ${deletingArray.length}`);
+  return true;
 };
 
 console.log("You are going to remove these friends: ", listOfFriendsToDelete);
